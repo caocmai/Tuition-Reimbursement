21515a61caf322138c89df1cf682491ec0af813c
caocmai <caomai@rocketmail.com>
2021-07-14 21:33:21 -0700

    Initialize project with Maven

---
+++ src/main/java/dev/mai/tests/Tests.java
@@ -0,0 +1,14 @@
+package dev.mai.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class Tests {
+	
+	@Test
+	public void testExample() {
+		assertEquals(3, 3);
+	}
+
+}
