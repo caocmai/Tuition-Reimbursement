ab4b9de6fa864102af78a8a311796a7f7db5add9
caocmai <caomai@rocketmail.com>
2021-07-19 21:16:24 -0700

    Add Hibernate dependencies

--- src/main/java/dev/mai/app/App.java
+++ src/main/java/dev/mai/app/App.java
@@ -1,9 +1,15 @@
 package dev.mai.app;
 
+import org.apache.log4j.Logger;
+
 public class App {
+	
+	final static Logger log = Logger.getLogger(App.class);
+
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
 	}
 
 }
